c9dcf10847a71aad1730e7197a306a03fc4ee8a1
Elliot D <prophet.18@rbx.life>
2023-10-14 18:28:10 -0400

    add icons
    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "coleccion.android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "coleccion.android"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
