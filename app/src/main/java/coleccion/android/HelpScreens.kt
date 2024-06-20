49c790ef3ca7453bffbd71f4900053943a59451d
Elliot D <prophet.18@rbx.life>
2024-06-20 18:49:43 -0400

    help screen redesign
    designs

--- app/src/main/java/coleccion/android/HelpScreens.kt
+++ app/src/main/java/coleccion/android/HelpScreens.kt
@@ -1,5 +1,6 @@
 package coleccion.android
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
@@ -11,22 +12,29 @@ class HelpScreens : ComponentActivity() {
     private lateinit var eButto2 : ImageButton
     private lateinit var eButto22 : ImageButton
     private lateinit var eButto3 : ImageButton
+    private lateinit var eButto222 : ImageButton
+    private lateinit var eButto32 : ImageButton
     private lateinit var eButto33 : ImageButton
     private lateinit var viewFlip : ViewFlipper
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.help_screens)
+        setContentView(R.layout.help_layout)
 
         viewFlip = findViewById(R.id.helpFlipper)
         eButto2 = findViewById(R.id.e_button2)
         eButto22 = findViewById(R.id.e_button22)
         eButto3 = findViewById(R.id.e_button3)
+        eButto222 = findViewById(R.id.e_button222)
+        eButto32 = findViewById(R.id.e_button32)
         eButto33 = findViewById(R.id.e_button33)
 
         eButto2.setOnClickListener { returningHome() }
         eButto22.setOnClickListener { viewFlip.showNext() }
         eButto3.setOnClickListener { viewFlip.showPrevious() }
+        eButto222.setOnClickListener { viewFlip.showNext() }
+        eButto32.setOnClickListener { viewFlip.showPrevious() }
         eButto33.setOnClickListener { returningHome() }
     }
 
